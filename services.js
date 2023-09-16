302d761315e11e4529d62af0acd8e00a27bea695
Gage Geigle <gage@gagegeigle.com>
2023-09-16 07:10:23 -0500

    Updated services to consume the new backend.

--- services.js
+++ services.js
@@ -9,7 +9,7 @@ weatherApp.service('weatherService', ['$resource',  function($resource) {
 
     this.GetWeather = function(city, daycount) {
 
-        var weatherAPI = $resource('http://127.0.0.1:8080/wetter', {callback: "JSON_CALLBACK" }, +'&'+ { get: { method: "JSONP"}});
+        var weatherAPI = $resource('http://wetter-backend-production.up.railway.app/wetter', {callback: "JSON_CALLBACK" }, +'&'+ { get: { method: "JSONP"}});
     
         console.log(city + ', ' + daycount + ' days');
         return weatherAPI.get({city: city, days: daycount});
