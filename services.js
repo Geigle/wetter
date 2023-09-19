f5df17fb167ce02e7d7aa33a65d8cfe8a70f44e3
Gage Geigle <gage@gagegeigle.com>
2023-09-19 07:50:04 -0500

    weather api resource now  identified under a proper URL and HTTPS is supported.

--- services.js
+++ services.js
@@ -9,9 +9,9 @@ weatherApp.service('weatherService', ['$resource',  function($resource) {
 
     this.GetWeather = function(city, daycount) {
 
-        var weatherAPI = $resource('http://wetter-backend-production.up.railway.app/wetter', {callback: "JSON_CALLBACK" }, +'&'+ { get: { method: "JSONP"}});
+        var weatherAPI = $resource('https://wetter-backend.gagegeigle.net/wetter', {callback: "JSON_CALLBACK" }, +'&'+ { get: { method: "JSONP"}});
     
         console.log(city + ', ' + daycount + ' days');
         return weatherAPI.get({city: city, days: daycount});
     }   
-}])
+}]);
